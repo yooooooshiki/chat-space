b458ee7b8df01d352dbaf4f9dd891316cd9e56e9
小松原嘉起 <komatsubarayoshiki@komatsubarayoshimikinoMacBook-Pro.local>
2019-06-27 20:40:54 +0900

    complete

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -30,6 +30,7 @@ $(function() {
   
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
+    console.log(input);
     $.ajax({
       type: 'GET',
       url: '/users',
