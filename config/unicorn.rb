78c9728fe11b2f4c73e71dbb7becf6e5750a8bf9
小松原嘉起 <komatsubarayoshiki@komatsubarayoshimikinoMacBook-Pro.local>
2019-07-02 15:51:18 +0900

    fix unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,14 +1,14 @@
-app_path = File.expand_path('../../', __FILE__)
+# app_path = File.expand_path('../../', __FILE__)
 
-worker_processes 1
+# worker_processes 1
 
-working_directory app_path
-pid "#{app_path}/tmp/pids/unicorn.pid"
-listen "#{app_path}/tmp/sockets/unicorn.sock"
-stderr_path "#{app_path}/log/unicorn.stderr.log"
-stdout_path "#{app_path}/log/unicorn.stdout.log"
+# working_directory app_path
+# pid "#{app_path}/tmp/pids/unicorn.pid"
+# listen "#{app_path}/tmp/sockets/unicorn.sock"
+# stderr_path "#{app_path}/log/unicorn.stderr.log"
+# stdout_path "#{app_path}/log/unicorn.stdout.log"
 
- ↓↓↓↓↓↓↓ 以下のように変更 ↓↓↓↓↓↓
+#  ↓↓↓↓↓↓↓ 以下のように変更 ↓↓↓↓↓↓
 
 # ../が一つ増えている
 app_path = File.expand_path('../../../', __FILE__)
